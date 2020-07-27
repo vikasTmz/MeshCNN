0400c8fa89514ae1952a207e8933926d7f078c71
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-07-27 05:20:32 -0400

    new scripts

--- models/layers/mesh_prepare.py
+++ models/layers/mesh_prepare.py
@@ -307,8 +307,10 @@ def set_edge_lengths(mesh, edge_points=None):
     edge_lengths = np.linalg.norm(mesh.vs[edge_points[:, 0]] - mesh.vs[edge_points[:, 1]], ord=2, axis=1)
     mesh.edge_lengths = edge_lengths
     print("===================== Mesh edge vertex index: START ===========================================================")
+    f = open("temp.txt",'w')
     for i in range(len(edge_lengths)):
-        print(i, edge_points[i, 0], edge_points[i, 1], mesh.vs[edge_points[i, 0]], mesh.vs[edge_points[i, 1]])
+        f.write(mesh.vs[edge_points[i, 0]], mesh.vs[edge_points[i, 1]])
+    f.close()
     print("===================== Mesh edge vertex index: END ===========================================================")
 
 def extract_features(mesh):
