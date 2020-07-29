e70dfb7df12dc808af9423e32c117cee04ec4c64
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-07-29 12:51:50 -0400

    test to solve artifacts [13]

--- models/layers/mesh_prepare.py
+++ models/layers/mesh_prepare.py
@@ -323,7 +323,7 @@ def get_edge_colors(mesh):
     edge_colors = []
     edge_points = get_edge_points(mesh)
     edge_lengths = np.linalg.norm(mesh.vs[edge_points[:, 0]] - mesh.vs[edge_points[:, 1]], ord=2, axis=1)
-    print("edge_lengths: ", edge_lengths)
+    print("edge_lengths: ", len(edge_lengths))
     for i in range(len(edge_lengths)):
         avg_color = [(mesh.colors[edge_points[i, 0]][0] + mesh.colors[edge_points[i, 1]][0]) / 2, 
                      (mesh.colors[edge_points[i, 0]][1] + mesh.colors[edge_points[i, 1]][1]) / 2,
