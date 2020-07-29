7607bf6c9356019c7575c6eb71f1cee2e9531fa1
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-07-29 12:53:08 -0400

    test to solve artifacts [15]

--- scripts/texturize/train.sh
+++ scripts/texturize/train.sh
@@ -7,8 +7,8 @@ python train.py \
 --arch meshunet \
 --dataset_mode texturize \
 --ncf 32 64 128 256 \
---ninput_edges 2304 \
---pool_res 1700 1050 600 \
+--ninput_edges 2880 \
+--pool_res 2000 1200 600 \
 --resblocks 3 \
 --lr 0.001 \
 --batch_size 12 \
