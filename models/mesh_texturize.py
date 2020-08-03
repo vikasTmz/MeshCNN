9eec624dbea3dfb7b5225e3c83325746f04b3ec1
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-03 13:39:50 -0400

    cube test: new [2]

--- models/mesh_texturize.py
+++ models/mesh_texturize.py
@@ -49,7 +49,7 @@ class TexturizeModel:
         self.edge_features = input_edge_features.to(self.device).requires_grad_(self.is_train)
         self.labels = labels.to(self.device)
         self.mesh = data['mesh']
-        print(self.mesh)
+        print(self.mesh[0].vs)
 
 
     def forward(self):
