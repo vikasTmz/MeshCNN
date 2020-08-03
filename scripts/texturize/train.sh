bf05d767e255fdaf191963b004e89c33eb369ea2
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-03 18:21:24 -0400

    save results to obj: [40]

--- scripts/texturize/train.sh
+++ scripts/texturize/train.sh
@@ -7,8 +7,8 @@ python train.py \
 --arch meshunet \
 --dataset_mode texturize \
 --ncf 32 64 128 256 \
---ninput_edges 18 \
---pool_res 18 18 18 \
+--ninput_edges 5331 \
+--pool_res 4600 3800 3000 \
 --resblocks 3 \
 --lr 0.001 \
 --batch_size 12 \
