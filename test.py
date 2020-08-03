dedc5cd315c44cae7cd0bf4a7e7227f57230c574
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-03 17:41:19 -0400

    save results to obj: [31]

--- test.py
+++ test.py
@@ -28,6 +28,7 @@ def run_test(epoch=-1):
         gt_vcolor = np.zeros(mesh[0].vs.shape, dtype=np.float32) * -1
         out_vcolor = np.zeros(mesh[0].vs.shape, dtype=np.float32) * -1
         for i, edges in mesh[0].edges:
+            print(edges, i)
             gt_vcolor[edges[0]] = update_vertex_color(gt_vcolor[edges[0]], gt[i])
             gt_vcolor[edges[1]] = update_vertex_color(gt_vcolor[edges[1]], gt[i])
 
