bf3afb8db6b2d0eba73d94a3b24b9ebdf0b8feb0
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-03 12:51:53 -0400

    test to solve artifacts : cube

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -29,17 +29,14 @@ class BaseDataset(data.Dataset):
             num_aug = self.opt.num_aug
             self.opt.num_aug = 1
             mean, std = np.array(0), np.array(0)
-            count = 0 
             for i, data in enumerate(self):
                 if i % 500 == 0:
                     print('{} of {}'.format(i, self.size))
                 features = data['edge_features']
                 mean = mean + features.mean(axis=1)
                 std = std + features.std(axis=1)
-                count = i
-            mean = mean / (count + 1)
-            std = std / (count + 1)
-            print("mean: ",mean)
+            mean = mean / (i + 1)
+            std = std / (i + 1)
             transform_dict = {'mean': mean[:, np.newaxis], 'std': std[:, np.newaxis],
                               'ninput_channels': len(mean)}
             with open(mean_std_cache, 'wb') as f:
