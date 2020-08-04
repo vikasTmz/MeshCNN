cc0624648696af38f1925bc1cf8afd762f75d6ea
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-03 22:19:06 -0400

    experimenting with diff pools

--- scripts/texturize/train.sh
+++ scripts/texturize/train.sh
@@ -8,12 +8,12 @@ python train.py \
 --dataset_mode texturize \
 --ncf 32 64 128 256 \
 --ninput_edges 1728 \
---pool_res 1728 1728 \
+--pool_res 1728 1728 1728 \
 --resblocks 3 \
 --lr 0.001 \
 --batch_size 12 \
---num_aug 1 \
---slide_verts 0 \
+--num_aug 10 \
+--slide_verts 0.2 \
 
 
 #
