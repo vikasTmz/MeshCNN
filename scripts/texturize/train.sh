0c1d91873fbda979892e31f871482552b4c1d469
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-04 03:51:28 -0400

    experimenting with diff pools

--- scripts/texturize/train.sh
+++ scripts/texturize/train.sh
@@ -7,8 +7,8 @@ python train.py \
 --arch meshunet \
 --dataset_mode texturize \
 --ncf 32 64 128 256 \
---ninput_edges 16416 \
---pool_res 16416 16416 16416 \
+--ninput_edges 1547 \
+--pool_res 1547 1547 1547 \
 --resblocks 3 \
 --lr 0.001 \
 --batch_size 12 \
