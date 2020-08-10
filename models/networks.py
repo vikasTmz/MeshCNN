8606f8abea0df927e004b40ac3e8e961536e5a38
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-10 18:32:04 -0400

    experimenting with diff pools

--- models/networks.py
+++ models/networks.py
@@ -116,7 +116,8 @@ def define_loss(opt):
     elif opt.dataset_mode == 'segmentation':
         loss = torch.nn.CrossEntropyLoss(ignore_index=-1)
     elif opt.dataset_mode == "texturize":
-        loss = torch.nn.L1Loss()
+        print("Using L2 Loss")
+        loss = torch.nn.L2Loss()
     return loss
 
 ##############################################################################
