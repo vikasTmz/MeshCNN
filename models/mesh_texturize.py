9a28dccccdf342f83333025ecd9f60e96f788956
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-08-11 09:19:09 -0400

    changed to sgd

--- models/mesh_texturize.py
+++ models/mesh_texturize.py
@@ -35,7 +35,10 @@ class TexturizeModel:
         self.criterion = networks.define_loss(opt).to(self.device)
 
         if self.is_train:
-            self.optimizer = torch.optim.Adam(self.net.parameters(), lr=opt.lr, betas=(opt.beta1, 0.1))
+            # SGD
+            self.optimizer = torch.optim.SGD(self.net.parameters(), lr=opt.lr, momentum=0.9)
+            # Adam
+            # self.optimizer = torch.optim.Adam(self.net.parameters(), lr=opt.lr, betas=(opt.beta1, 0.4))
             self.scheduler = networks.get_scheduler(self.optimizer, opt)
             print_network(self.net)
 
