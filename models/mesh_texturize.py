e5106ba4043dcabf4fceb5884f787f2fd579746f
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-09-02 09:50:06 -0400

    tv loss with 4 neighbours

--- models/mesh_texturize.py
+++ models/mesh_texturize.py
@@ -68,9 +68,8 @@ class TexturizeModel:
         tv_loss = 0
         # Perform shift difference for height, -height, width, -width i.e all 4 neighbours
         # if (self.loss * self.opt.lambda_L1).item() < 15:
-        print(dir(self.mesh))
-        gt_gemm = np.stack((self.labels[self.mesh.tvloss_edges[:,0]], self.labels[self.mesh.tvloss_edges[:,1]]), axis=1)
-        out_gemm = np.stack((out[self.mesh.tvloss_edges[:,0]], out[self.mesh.tvloss_edges[:,1]]), axis=1)
+        gt_gemm = np.stack((self.labels[self.mesh[0].tvloss_edges[:,0]], self.labels[self.mesh[0].tvloss_edges[:,1]]), axis=1)
+        out_gemm = np.stack((out[self.mesh[0].tvloss_edges[:,0]], out[self.mesh[0].tvloss_edges[:,1]]), axis=1)
         print(out.shape, gt_gemm.shape, out_gemm.shape)
         # _, height, chan = self.labels.shape
         # dy = torch.abs(self.labels[:,1:,:] - self.labels[:,:-1,:])
