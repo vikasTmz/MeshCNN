2e0a0d48504137ed9d597033bbef9d2d434fd795
vikasTmz <vikas.thamizharasan@students.iiit.ac.in>
2020-09-18 14:02:35 -0400

    testing irregularity

--- scripts/texturize/train.sh
+++ scripts/texturize/train.sh
@@ -8,7 +8,7 @@ python train.py \
 --dataset_mode texturize \
 --ncf 32 64 128 256 \
 --ninput_edges 1924 \
---pool_res 1924 1924 1924 \
+--pool_res 1924 1443 962 \
 --resblocks 3 \
 --lr 0.001 \
 --batch_size 12 \
